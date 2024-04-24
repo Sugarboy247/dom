66769eb343dce0e1c918e6a90e3017e22829b6f9
SugarBoy <oladipupotaiwoolawale@gmail.com>
2024-04-24 12:54:08 +0100

    rough

---
+++ main.js
@@ -0,0 +1,146 @@
+// let challenge = '30 Days Of JavaScript';
+// console.log(challenge)
+
+// let lenght = challenge.length;
+// console.log(lenght)
+
+// let upperCase = challenge.toUpperCase()
+// console.log(upperCase)
+
+// let lowerCase = challenge.toLowerCase()
+// console.log(lowerCase)
+
+// let slice = challenge.slice(2)
+// console.log(slice)
+
+// let include = challenge.includes('Script');
+// console.log(include)
+
+// let split = challenge.split();
+// console.log(split)
+
+// let split1 = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'.split(',')
+// console.log(split1)
+
+// let replace = challenge.replace('JavaScript', 'Python')
+// console.log(replace)
+
+// let char = challenge.charAt(15)
+// console.log(char)
+
+// let num = challenge.charCodeAt('J')
+// console.log(num)
+
+// let num1 = challenge.indexOf('a')
+// console.log(num1)
+
+// let index = challenge.lastIndexOf('a')
+// console.log(index)
+
+// let b = 'You cannot end a sentence with because because because is a conjunction'
+// const i = b.indexOf('because');
+
+// console.log(i)
+
+// const last = b.lastIndexOf('because')
+// console.log(last)
+
+// const search = b.search('because')
+// console.log(search)
+
+// const trim = challenge.trim()
+// console.log(trim)
+
+// const string = challenge.startsWith('')
+// console.log(string)
+
+// const ends = challenge.endsWith('')
+// console.log(ends)
+
+// const find = challenge.match('a')
+// console.log(find)
+
+// const repeat = challenge.repeat(2);
+// console.log(repeat)
+
+// console.log('There is no exercise better for the heart than reaching down and lifting people up')
+
+// console.log("Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.")
+
+
+// if (typeof '10' === 10){
+//     console.log(true)
+// }  else {
+//     console.log(false)
+// }
+
+// let parse = parseFloat('9.8')
+// if (parse !== 10){
+//     return parse = 10
+// }
+// console.log(parse)
+
+// let Python = 'python'
+// let jargon = 'jargon'
+
+// if (Python.indexOf('on') !== -1){
+//     console.log("'on' is found in python")
+// } else {
+//     console.log("'on' is not found in python")
+// }
+// if (jargon.indexOf('on') !== -1){
+//     console.log("'on' is found in jargon")
+// } else {
+//     console.log("'on' is not found in jargon")
+// }
+
+// let num = Math.floor(Math.random() * 101);
+
+// console.log(num)
+
+// let random = Math.floor(Math.random() * 51) + 50;
+
+// console.log(random)
+
+// let randomNumber = Math.floor(Math.random() * 226);
+
+// console.log(randomNumber)
+
+// let stri = 'javaScript'
+
+// let stringLength = Math.floor(Math.random() * stri.length)
+
+// let int = stri.charAt(stringLength)
+
+// console.log(int)
+
+// console.log('1\t1\t1\t1\t1');
+// console.log('2\t1\t2\t4\t8');
+// console.log('3\t1\t3\t9\t27');
+// console.log('4\t1\t4\t16\t64');
+// console.log('5\t1\t5\t25\t125');
+
+// let sentence = 'You cannot end a sentence with because because because is a conjunction'
+
+// let rel = sentence.indexOf('because')
+
+// let rel2 = sentence.indexOf('because', rel + 1)
+
+// const remove = sentence.substr(rel, rel2 - rel + 'because'.length)
+
+// console.log(remove)
+
+let msg = 'Love is the best thing in this world. Some found their love and some are still looking for their love.';
+
+let words = msg.split(' ');
+
+let count = 0;
+
+for ( let i = 0; i < words.length; i++){
+    let num = words[i].toLowerCase();
+
+    if (num === 'love'){
+        count++
+    }
+}
+console.log("loveCount:", count);
